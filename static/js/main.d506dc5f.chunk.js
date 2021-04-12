f4d76ab2f6b1718fc19b0c60b2fb1f285a48cb3f
Aamer Shikari <shikariaamer@gmail.com>
2021-04-11 21:30:03 -0400

    Updates

--- static/js/main.d506dc5f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpwebdev-6"]=this["webpackJsonpwebdev-6"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),s=n.n(c),r=n(3),i=n.n(r),o=(n(8),n(9),n(0));var a=function(){return Object(o.jsx)("div",{className:"App"})};i.a.render(Object(o.jsx)(s.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.d506dc5f.chunk.js.map
