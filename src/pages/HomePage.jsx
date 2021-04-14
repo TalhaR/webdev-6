0abf86bccdf828b71d1c09dbd03736db09b1f0be
xshi0603 <xshi603@gmail.com>
2021-04-14 01:48:12 -0400

    added button to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,10 +10,13 @@ function HomePage() {
                         <h1>Cats or Dogs?</h1>
                         <button type="button" className="btn btn-light">
                             <Link to="/cats">Cats</Link>
-                        </button>
+                        </button> <br/>
                         <button type="button" className="btn btn-light">
-                            Dogs
-                        </button>
+                            <Link to="/dogs">Dogs</Link>
+                        </button> <br/>
+                        <button type="button" className="btn btn-light">
+                            <Link to="/catfacts">Cat Facts</Link>
+                        </button> <br/>
                     </div>
                 </div>
             </div>
