3b560c7ae472edbd27caaf18a08da6cb20763885
Darren Liang <liangdarren2@gmail.com>
2021-04-18 09:43:12 -0400

    added dog pics api

--- src/pages/DogPage.jsx
+++ src/pages/DogPage.jsx
@@ -2,12 +2,26 @@ import React, { useEffect, useState } from 'react'
 const axios = require('axios');
 
 function DogPage() {
+    const [imgUrl, setImage] = useState(null)
+    useEffect(() => {
+        const search = async () => {
+            let res = await axios.get('https://dog.ceo/api/breeds/image/random')
+            
+            if (res.status === 200) {
+                setImage(res.data.message)
+            }
+        }
+        
+        search();
+    }, [])
     return (
         <>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 text-center align-items-center">
                         <h1>Dogs</h1>
+
+                        <img src={imgUrl} alt="Dog Pic" />
                     </div>
                 </div>
             </div>
